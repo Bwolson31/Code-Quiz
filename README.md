297bdea5120e29bac9700c802b59553ffc99e5b5
Brian Olson <bwolson31@gmail.com>
2024-01-23 22:06:20 -0600

    Added html, css, javascript, as well as a readme file

--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # Code-Quiz
+
+
+## Description 
+
+This JavaScript code allows users to take a 60 second 10 question quiz about programming, where incorrect answers penalize the user by 10 seconds. 
+
+## Installation 
+N/A
+
+## Usage
+To use this website, click the start button, and then answer the following 10 questions, at which point you should be able to submit your initials and see if you received a high score. 
+
+## Credits
+
+I created the JavaScript code
+
+## License
+
+MIT License
+
+Copyright (c) 2024 Bwolson31
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,161 +1,209 @@
 
-
-var timeEl = document.querySelector(".time");
-var quizEl = document.querySelector(".quiz");
-var quiz = document.querySelector(".quizBox");
-// const questionText = document.querySelector(".questionText");
-var secondsLeft = 60; 
-var startbtn = document.querySelector(".start");
-
-
-
+// questions
 
 const quizQuestions = [
-    {question: "What are the types of data in JavaScript?",
-possibleAnswers: ["1: strings, numbers, and booleans",
-    "2: strings, numbers, booleans, undefined, and null",
-    "3: strings, numbers, booleans, undefined, null, objects, and arrays",
-    "4: strings, numbers, booleans, objects, and arrays"],
-correctAnswer: 2,
-incorrectAnswers: [0, 1, 3] 
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: incorrect",
-    "4: correct"],
-correctAnswer: 3,
-incorrectAnswers: [0, 1, 2, 3]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: correct",
-    "4: incorrect"],
-correctAnswer: 2,
-incorrectAnswers: [0, 1, 3]
-},
-{question: "question",
-possibleAnswers: ["1: correct",
-    "2: incorrect",
-    "3: incorrect",
-    "4: incorrect"],
-correctAnswer: 0,
-incorrectAnswers: [1, 2, 3,]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: incorrect",
-    "4: correct"],
-correctAnswer: 3,
-incorrectAnswers: [0, 1, 2]
-},
-{question: "question",
-possibleAnswers: ["1: correct",
-    "2: incorrect",
-    "3: incorrect",
-    "4: incorrect"],
-correctAnswer: 0,
-incorrectAnswers: [1, 2, 3,]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: correct",
-    "3: incorrect",
-    "4: incorrect"],
-correctAnswer: 1,
-incorrectAnswers: [0, 2, 3]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: incorrect",
-    "4: correct"],
-correctAnswer: 0,
-incorrectAnswers: [0, 1, 2, 3]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: incorrect",
-    "4: correct"],
-correctAnswer: 3,
-incorrectAnswers: [0, 1, 2]
-},
-{question: "question",
-possibleAnswers: ["1: incorrect",
-    "2: incorrect",
-    "3: correct",
-    "4: incorrect"],
-correctAnswer: 2,
-incorrectAnswers: [0, 1, 3]
-},]
+  {
+    question: 'What are the types of data in JavaScript?',
+    possibleAnswers: [
+      '1: strings, numbers, and booleans',
+      '2: strings, numbers, booleans, undefined, and null',
+      '3: strings, numbers, booleans, undefined, null, objects, and arrays',
+      '4: strings, numbers, booleans, objects, and arrays',
+    ],
+    correctAnswer: 2,
+    incorrectAnswers: [0, 1, 3],
+  },
+  {
+    question: 'Arrays in JavaScript can be used to store _____',
+    possibleAnswers: [
+      '1: other arrays',
+      '2: booleans',
+      '3: numbers and strings',
+      '4: all of the above',
+    ],
+    correctAnswer: 3,
+    incorrectAnswers: [0, 1, 2, 3],
+  },
+  {
+    question: 'String values must be enclosed within _____ when being assigned to variables.',
+    possibleAnswers: [
+      '1: square brackets',
+      '2: curly brackets',
+      '3: quotes',
+      '4: parentheses',
+    ],
+    correctAnswer: 2,
+    incorrectAnswers: [0, 1, 3],
+  },
+  {
+    question: 'To match a specific class attribute, what punctuation mark do you use to start a selector?',
+    possibleAnswers: [
+      '1: .',
+      '2: ,',
+      '3: #',
+      '4: ;',
+    ],
+    correctAnswer: 0,
+    incorrectAnswers: [1, 2, 3],
+  },
+  {
+    question: 'To match a specific Id attribute, what punctuation mark to you use to start a selector?',
+    possibleAnswers: [
+      '1: .',
+      '2: ,',
+      '3: ;',
+      '4: #',
+    ],
+    correctAnswer: 3,
+    incorrectAnswers: [0, 1, 2],
+  },
+  {
+    question: 'A very useful tool during development and debugging for printing content to the debugger is:',
+    possibleAnswers: [
+      '1: console.log',
+      '2: JavaScript',
+      '3: terminal',
+      '4: for loops',
+    ],
+    correctAnswer: 0,
+    incorrectAnswers: [1, 2, 3],
+  },
+  {
+    question: 'The condition of an if/else statement is enclosed within ______',
+    possibleAnswers: [
+      '1: Quotes',
+      '2: Parentheses',
+      '3: Curly Brackets',
+      '4: Square Brackets',
+    ],
+    correctAnswer: 1,
+    incorrectAnswers: [0, 2, 3],
+  },
+  {
+    question: 'What is the following output of code: console.log(3 + "3")',
+    possibleAnswers: [
+      '1: 6',
+      '2: NaN',
+      '3: 9',
+      '4: 33',
+    ],
+    correctAnswer: 0,
+    incorrectAnswers: [0, 1, 2, 3],
+  },
+  {
+    question: 'What does the Math.random() function do in JavaScript',
+    possibleAnswers: [
+      '1: It doesnt do anything.',
+      '2: It rounds a number down to the nearest integer.',
+      '3: It creates any random number.',
+      '4: It generates a random floating-point number between 0 and 1.',
+    ],
+    correctAnswer: 3,
+    incorrectAnswers: [0, 1, 2],
+  },
+  {
+    question: 'How many pixels is 1em?',
+    possibleAnswers: [
+      '1: 41px;',
+      '2: 32px;',
+      '3: 16px;',
+      '4: 21px;',
+    ],
+    correctAnswer: 2,
+    incorrectAnswers: [0, 1, 3],
+  },
+];
 
-function startQuiz(){
-    document.getElementsByClassName("quizContainer").style.display="block";
-}
 
 
-
+let secondsLeft = 60;
 let currentQuestion = 0;
 let score = 0;
-score =+
 
+function startQuiz() {
+  // hide our starter container and show our quiz container
+  document.getElementById('landing').style.display = 'none';
+  document.querySelector('.container').style.display = 'block';
+
+  // start our timer / countdown
+ 
+  var timerInterval = setInterval(function () {
+    const timeEl = document.querySelector('.time');
+    secondsLeft--;
+    if (secondsLeft > 0) {
+      timeEl.textContent = 'Time: ' + secondsLeft;
+    } else {
+      timeEl.textContent = '';
+    }
+
+    if (currentQuestion > quizQuestions.length - 1 || secondsLeft <= 0) {
+      clearInterval(timerInterval);
+      const container = document.querySelector('.container');
+      container.innerHTML = `<p>All done! You got ${score} out of ${quizQuestions.length} questions correct.<p>`;
+
+      // show our high score form
+      document.querySelector('.highScore').style.display = 'block';
+    }
+  }, 1000);
+
+  showQuestion();
+}
 
 function showQuestion() {
-    const questionText = document.getElementById("questions");
-    questionText.textContent = quizQuestions[currentQuestion].question;
+  const questionText = document.getElementById('questions');
+  questionText.textContent = quizQuestions[currentQuestion].question;
 
-
-const possibleAnswers = document.querySelectorAll(".answer");
-possibleAnswers.forEach((answer, index) => {
+  const possibleAnswers = document.querySelectorAll('.answer');
+  possibleAnswers.forEach((answer, index) => {
     answer.textContent = quizQuestions[currentQuestion].possibleAnswers[index];
-});
+  });
 
-const response = document.getElementById("response");
-response.textContent = "";
+  const response = document.getElementById('response');
 }
 
 function evaluateAnswer(selected) {
-    const response = document.getElementById("response");
-    if (selected === quizQuestions[currentQuestion].correctAnswer) {
-        response.textContent ="Correct!";
-        score++;
+  const response = document.getElementById('response');
 
-    } else {response.textContent = "Incorrect!";
+  if (selected === quizQuestions[currentQuestion].correctAnswer) {
+    response.textContent = 'Correct!';
+    score++;
+  } else {
+    response.textContent = 'Incorrect!';
+    secondsLeft -= 10;
+  }
+
+  currentQuestion++;
+  showQuestion();
+}
+
+
+// This section is unfinished
+
+function submitHighScore() {   
+var initialsInput = document.getElementById('initials').value.trim();
+if (initialsInput !== '') {
+    var savedScore = JSON.parse(window.localStorage.getItem('initialsInput', 'score')) || [];   
+} 
+
+
+
+var userInitials = {
+    userScore: savedScore,
+}
+
+window.localStorage.getItem('userInitials', JSON.stringify(userInitials));
+
+// create a new object userInitials maybe to put save userInitials to local storage and stringify it. Direct it to next page/area. 
+
+
+localStorage.setItem('initialsInput', 'score');
+
+
 }
 
 
 
-    var timerInterval = setInterval(function() {
-        currentQuestion++; 
-        secondsLeft--;
-        timeEl.textContent = "Time: " + secondsLeft;
-        timeEl.forEach(IncorrectAnswers) = - 10;
 
 
-
-        if(secondsLeft > 0) {
-            showQuestion();
-        
-        } if (!currentQuestion < quizQuestions.length || secondsLeft === 0) { 
-            clearInterval(timerInterval);
-            const container = document.querySelector(".container");
-        container.innerHTML = `<p>All done!. You got ${score} out of ${quizQuestions.length} questions correct.<p>`
-    }
-        }, 1000);
-}
-
-
-
-showQuestion();
-
-var highScore = JSON.parse(localStorage.getItem) ("")
-
-
-
-
-localStorage.getItem("highScore");
 
 
